ac43d294ed969c8c8cc07d6b1c39c9e5ef27b67f
Muhammad <muhdmahmood@gmail.com>
2023-08-04 05:43:13 +0800

    redner css files, and data unto client side

---
+++ app/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('post/<slug:slug>', views.post_page, name='post_page'),
+    path('', views.index, name="index"),
+
+]
